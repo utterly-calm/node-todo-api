0881cf0996d6919523c39ad4b7fc35d1f59c0893
manu1222 <manu.sharma1222@gmail.com>
2018-01-16 16:05:15 +0530

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { mongoose } = require("./db/mongoose");
-const { Todo } = require("./models/Todo");
-const { User } = require("./models/User");
+const { Todo } = require("./models/todo");
+const { User } = require("./models/user");
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,11 +13,14 @@ app.post("/todos", (req, res) => {
     text: req.body.text
   });
 
-  todo.save().then(doc => {
-    res.send(doc);
-  }, e => {
-    res.status(400).send(e);
-  });
+  todo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
 });
 
 app.get("/todos", () => {});
@@ -25,3 +28,5 @@ app.get("/todos", () => {});
 app.listen(3000, () => {
   console.log("Stated on port 3000.");
 });
+
+module.exports = { app };
