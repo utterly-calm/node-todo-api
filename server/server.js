4e256eeedb10d17eb1eb74c7a01986600600df08
manu1222 <manu.sharma1222@gmail.com>
2018-01-17 15:51:44 +0530

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -58,6 +58,25 @@ app.get(
   }
 );
 
+app.delete("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  console.log(id);
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send(todo);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
+});
+
+
 app.listen(port, () => {
   console.log(`Stated on port ${port}.`);
 });
