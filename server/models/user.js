597d43c2fd518119b5e7d413dadaa848fef83ed7
manu1222 <manu.sharma1222@gmail.com>
2018-01-19 17:35:43 +0530

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -61,6 +61,15 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
+UserSchema.methods.removeToken = function(token) {
+  var user = this;
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  })
+}
+
 UserSchema.statics.findByToken = function(token) {
   var User = this;
   var decoded;
--- server/server.js
+++ server/server.js
@@ -137,6 +137,17 @@ app.post("/users/login", (req, res) => {
     });
 });
 
+app.delete("/users/me/token", authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(
+    () => {
+      res.status(200).send();
+    },
+    e => {
+      res.status(400).send();
+    }
+  );
+});
+
 app.listen(port, () => {
   console.log(`Stated on port ${port}.`);
 });
