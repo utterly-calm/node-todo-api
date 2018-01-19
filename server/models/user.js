f1eb703e4450458faf790841d94292f32d5bece6
manu1222 <manu.sharma1222@gmail.com>
2018-01-19 12:42:13 +0530

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const _ = require("lodash");
+const bcrypt = require("bcryptjs");
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -69,11 +70,25 @@ UserSchema.statics.findByToken = function(token) {
     return Promise.reject();
   }
   return User.findOne({
-    '_id': decoded._id,
-    'tokens.access': 'auth',
-    'tokens.token': token
+    _id: decoded._id,
+    "tokens.access": "auth",
+    "tokens.token": token
   });
 };
+
+UserSchema.pre("save", function(next) {
+  var user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model("User", UserSchema);
 module.exports = { User };
