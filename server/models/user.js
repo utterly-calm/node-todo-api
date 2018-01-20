ebc7c895b72dd649c2dbba8d676a98d2761cae51
manu1222 <manu.sharma1222@gmail.com>
2018-01-20 23:33:43 +0530

    Update config to use JSON file

--- server/models/user.js
+++ server/models/user.js
@@ -51,7 +51,7 @@ UserSchema.methods.generateAuthToken = function() {
         _id: user._id.toHexString(),
         access
       },
-      "abc123"
+      process.env.JWT_SECRET
     )
     .toString();
 
@@ -75,7 +75,7 @@ UserSchema.statics.findByToken = function(token) {
   var User = this;
   var decoded;
   try {
-    decoded = jwt.verify(token, "abc123");
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     return Promise.reject();
   }
--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -14,7 +14,7 @@ const users = [
     tokens: [
       {
         access: "auth",
-        token: jwt.sign({ _id: userOneId, access: "auth" }, "abc123").toString()
+        token: jwt.sign({ _id: userOneId, access: "auth" }, process.env.JWT_SECRET).toString()
       }
     ]
   },
@@ -25,7 +25,7 @@ const users = [
     tokens: [
       {
         access: "auth",
-        token: jwt.sign({ _id: userTwoId, access: "auth" }, "abc123").toString()
+        token: jwt.sign({ _id: userTwoId, access: "auth" }, process.env.JWT_SECRET).toString()
       }
     ]
   }
